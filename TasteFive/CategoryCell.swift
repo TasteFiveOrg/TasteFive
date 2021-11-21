683eaf7a253283697a4639e1daa104c227bc468b
James Zou <74443409+Jams1223@users.noreply.github.com>
2021-11-21 11:47:06 -0500

    adding Categories

---
+++ TasteFive/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  TasteFive
+//
+//  Created by James Zou on 11/21/21.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TasteFive/FeedViewController.swift
+++ TasteFive/FeedViewController.swift
@@ -6,15 +6,64 @@
 //
 
 import UIKit
-class FeedViewController: UIViewController {
+import Parse
+
+
+class FeedViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var categories = [PFObject]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as! CategoryCell
+        
+        let category = categories[indexPath.row]
+        
+        cell.categoryLabel.text = category["CategoryName"] as! String
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let query = PFQuery(className: "Categories")
+        
+        query.findObjectsInBackground{(Categories,error) in
+            if Categories != nil{
+                self.categories = Categories!
+                self.tableView.reloadData()
+            }
+            
+        }
+    }
+    
+  
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func onLogOutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard (name: "Main", bundle: nil)
+        
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
     /*
     // MARK: - Navigation
 

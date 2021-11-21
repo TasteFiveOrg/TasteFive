40bdc3553d18099ce84e35d7d37ecc4be759f464
James Zou <74443409+Jams1223@users.noreply.github.com>
2021-11-20 19:39:12 -0500

    Commit 2 for User can login/Register
    Reinstalled Pod to solve bug
    Added Parse
    added user log in and register features

---
+++ Pods/Target Support Files/Pods-TasteFive-TasteFiveUITests/Pods-TasteFive-TasteFiveUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TasteFive_TasteFiveUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TasteFive_TasteFiveUITests
+@end
--- TasteFive/LoginViewController.swift
+++ TasteFive/LoginViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import Parse
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,9 +21,34 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password){
+            (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else{
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
     }
     
     @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground{
+            (success,error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else{
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
     }
     /*
     // MARK: - Navigation

b4dfe3db00f65e05f1e167fd03f83f3a6fca8e1a
abdullahsal702 <88620119+abdullahsal702@users.noreply.github.com>
2021-12-04 13:44:02 -0500

    profile collection view

---
+++ TasteFive/ProfileGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileGridCell.swift
+//  TasteFive
+//
+//  Created by Abdullah Saleh on 12/4/21.
+//
+
+import UIKit
+
+class ProfileGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var ingredientLabel: UILabel!
+    @IBOutlet weak var instructionsText: UITextView!
+}
--- TasteFive/ProfileViewController.swift
+++ TasteFive/ProfileViewController.swift
@@ -7,15 +7,70 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+import AlamofireImage
 
+import Parse
+
+
+
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    var post: PFObject!
+    var categorie: PFObject!
+    var posts = [PFObject]()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing) / 2
+        layout.itemSize = CGSize(width: width, height: width * 3/2)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileGridCell", for: indexPath) as! ProfileGridCell
+        
+        let post = posts[indexPath.item]
+        
+        cell.recipeTitleLabel.text = post["title"] as? String
+        cell.instructionsText.text = post["instructionsText"] as? String
+        cell.ingredientLabel.text = post["ingredients"] as? String
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+
+        cell.recipeImage.af_setImage(withURL: url)
+
+        return cell
+
+    }
+    
     /*
     // MARK: - Navigation
 
